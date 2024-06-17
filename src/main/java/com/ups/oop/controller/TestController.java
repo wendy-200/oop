49ce9e554a52127b474654863f6c1bd052024103
Wendy Ortiz <Wortizm2@est.ups.edu.ec>
2024-06-17 09:33:58 -0500

    Add(no_of_commit): commit message

--- src/main/java/com/ups/oop/controller/TestController.java
+++ src/main/java/com/ups/oop/controller/TestController.java
@@ -15,10 +15,12 @@ public class TestController {
 
     @GetMapping("/hello")
     public String hello(@RequestParam String name,
-                        @RequestParam String lastname,
+                        @RequestParam(required = false) String lastname,
                         @RequestParam(required = false) Integer age) {
         String message = "This is my first SpringBootProject!" +
-                ", and my name is: " + name + " " + lastname;
+                ", and my name is: " + name;
+        if (lastname != null)
+            message += " " + lastname;
         if (age != null)
             message += ", and my age is: " + age + " years old";
         return message;
@@ -26,18 +28,19 @@ public class TestController {
 
     @GetMapping("/concat/{name}/{lastname}/{age}")
     public String concatenate(@PathVariable String name,
-                              @PathVariable String lastname){
-        return "This is my second rest service!, and my name is: "
-        + name + " " + lastname;
-
+                              @PathVariable(required = false) String lastname,
+                              @PathVariable(required = false) String age) {
+        String message = "This is my second rest service!, and my name is: " + name;
+        if (lastname != null)
+            message += " " + lastname;
+        return message;
     }
 
-    @GetMapping("/concat/{name}/{lastname}/{age}")
-    public String concatenate(@PathVariable String name,
-                              @PathVariable String lastname,
-                              @PathVariable String age) {
-        return "This is my second rest service!, and my name is: "
-        + name + " " + lastname;
-
+    @GetMapping("/concat/{name}")
+    public String concatenate(@PathVariable String name) {
+        String message = "This is my second rest service!, and my name is";
+        if (name != null)
+            message += " " + name;
+        return message;
     }
 }
