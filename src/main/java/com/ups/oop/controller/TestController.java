06e60c4950ebaaaeb9a0eb13f854081e8b48ff1f
Wendy Ortiz <Wortizm2@est.ups.edu.ec>
2024-06-18 13:33:17 -0500

    Add(I):contacte

--- src/main/java/com/ups/oop/controller/TestController.java
+++ src/main/java/com/ups/oop/controller/TestController.java
@@ -14,30 +14,43 @@ public class TestController {
     }
 
     @GetMapping("/hello")
-    public String hello(@RequestParam String name,
-                        @RequestParam String lastname,
+    public String hello(@RequestParam (required = false)String name,
+                        @RequestParam(required = false) String lastname,
                         @RequestParam(required = false) Integer age) {
-        String message = "This is my first SpringBootProject!" +
-                ", and my name is: " + name + " " + lastname;
+        String message = "This is my first SpringBootProject!";
+        if (name != null)
+           message += " " + name;
+        if (lastname != null)
+            message += " " + lastname;
         if (age != null)
             message += ", and my age is: " + age + " years old";
         return message;
     }
 
-    @GetMapping("/concat/{name}/{lastname}/{age}")
-    public String concatenate(@PathVariable String name,
-                              @PathVariable String lastname){
-        return "This is my second rest service!, and my name is: "
-        + name + " " + lastname;
-
+    @GetMapping("/concat/")
+    public String concat(){
+        return "This is my second rest service!, and my name is";
     }
 
+
+
     @GetMapping("/concat/{name}/{lastname}/{age}")
     public String concatenate(@PathVariable String name,
-                              @PathVariable String lastname,
-                              @PathVariable String age) {
-        return "This is my second rest service!, and my name is: "
-        + name + " " + lastname;
+                              @PathVariable(required = false) String lastname,
+                              @PathVariable(required = false) String age) {
+        String message = "This is my second rest service!, and my name is: " + name;
+        if (lastname != null)
+            message += " " + lastname;
+        return message;
+    }
 
+    @GetMapping("/concat/{name}")
+    public String concatenate
+            (@PathVariable String name) {
+        String message = "This is my second rest service!, and my name is";
+        if (name != null)
+            message += " " + name;
+        return message;
     }
 }
+
