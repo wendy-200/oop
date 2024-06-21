4a325c10b4d7a302c72de2cd1464e61b698b5064
Wendy Ortiz <Wortizm2@est.ups.edu.ec>
2024-06-21 11:59:08 -0500

    Add(I): controller_Person

---
+++ src/main/java/com/ups/oop/Service/PersonService.java
@@ -0,0 +1,31 @@
+package com.ups.oop.Service;
+
+import com.ups.oop.dto.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PersonService {
+    private List<Person> personList = new ArrayList<>();
+
+    public Person createPerson(Person person) {
+        personList.add(person);
+        return person;
+    }
+
+    public List<Person> getAllPeople() {
+        return personList;
+    }
+
+    public Person getPersonById(String id) {
+        Person person = new Person();
+        for (Person per : personList){
+           if(id.equalsIgnoreCase(per.getId())){
+            return per;
+    }
+        }
+        return person;
+    }
+}
