7157bd80d3ac0c603a4dee4e34d1ed3aa3579600
Wendy Ortiz <Wortizm2@est.ups.edu.ec>
2024-08-03 21:05:34 -0500

    Add(I): Animal-Thymleaf-Template

--- src/main/java/com/ups/oop/controller/TemplateController.java
+++ src/main/java/com/ups/oop/controller/TemplateController.java
@@ -1,13 +1,27 @@
 package com.ups.oop.controller;
 
+import com.ups.oop.Service.PersonService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TemplateController {
+    private final PersonService personService;
+
+    public TemplateController(PersonService personService) {
+        this.personService = personService;
+    }
+
     @GetMapping("/template")
     public String getTemplate(Model model){
         return "template";
+    }
+
+    @GetMapping("/people")
+    public String getPeople(Model model) {
+        model.addAttribute("people", personService.getAllPeople());
+        return "person/list";
     }
 }
